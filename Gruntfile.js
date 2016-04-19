a72d29a6e083693e61bd453c025909884220753b
Christopher T. Herrera (Eefret) <eefretsoul@gmail.com>
2016-04-19 19:01:30 -0400

    work in progress

---
+++ Gruntfile.js
@@ -0,0 +1,6 @@
+/**
+ * Created by eefret on 4/16/16.
+ */
+module.exports = function(grunt) {
+    // Do grunt-related things in here
+};
--- main.js
+++ main.js
@@ -4,15 +4,27 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
+let mainWindow;
 
-var mainWindow = null;
+function createWindow () {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+}
 
-app.on('window-all-closed', function(){
-    if (process.platform != 'darwin'){
+function windowClosed() {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
-});
+}
 
-app.on('ready', function(){
-   mainWindow = new BrowserWindow({width: 800, height: 600});
+app.on('ready', createWindow);
+app.on('window-all-closed', windowClosed);
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
